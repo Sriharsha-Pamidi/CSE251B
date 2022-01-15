ed7d12f6180b79b712611b29174a3636b8aeb06d
Sri Harsha Pamidi <pamidisriharsha@gmail.com>
2022-01-14 16:32:28 -0800

    data.py changes

--- Assignment-1/src/traffic_reader.py
+++ Assignment-1/src/traffic_reader.py
@@ -2,11 +2,8 @@ import pickle
 
 
 def load_traffic(path, kind='train', subclass=None):
-    import os
-    import gzip
-    import numpy as np
 
-    t_file = "./data/train" + "_wb_" + kind +".p"
+    t_file = f"./{path}/train" + "_wb_" + kind + ".p"
 
     """Load traffic data from `path`"""
     with open(t_file, mode='rb') as f:
